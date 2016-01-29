da86a2750febc6ce52a00e63221faaff63f92491
Piotr Kurpik <piotr.kurpik@ocado.com>
2016-01-29 14:28:10 +0100

    fix linting

--- manifests/install.pp
+++ manifests/install.pp
@@ -7,15 +7,15 @@ class sysdig::install {
 
       include apt
       apt::source { 'sysdig':
-        location   => 'http://download.draios.com/stable/deb',
-        release    => 'stable-$(ARCH)/',
-        repos      => '',
-        key        => {
+        location => 'http://download.draios.com/stable/deb',
+        release  => 'stable-$(ARCH)/',
+        repos    => '',
+        key      => {
           'id'     => 'D27A72F32D867DF9300A241574490FD6EC51E8C4',
           'source' => 'https://s3.amazonaws.com/download.draios.com/DRAIOS-GPG-KEY.public',
         },
-        include    => {
-          'src'    => false
+        include  => {
+          'src' => false
         }
       }
 
